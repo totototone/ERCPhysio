01594591a909dc9261a1d9e96d548a43a1eea9d0
SamiaBous <samia.b@codeur.online>
2018-02-28 14:34:24 +0100

    dossier repository

---
+++ src/Repository/UserRepository.php
@@ -0,0 +1,16 @@
+<?php
+// src/Repository/UserRepository.php
+namespace App\Repository;
+
+use App\Entity\User;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+
+class UserRepository extends ServiceEntityRepository
+{
+    public function __construct(RegistryInterface $registry)
+    {
+        parent::__construct($registry, User::class);
+    }
+}
+?> 
